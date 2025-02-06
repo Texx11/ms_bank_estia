9840d0901cc677132fee803a7c93ce43c822c115
Texx11 <122284873+Texx11@users.noreply.github.com>
2025-02-06 12:15:11 +0100

    Implementation Circuit Breaker if customer is not up -> account should still work

--- Account/src/main/java/fr/estia/mbds/account/CustomerClient.java
+++ Account/src/main/java/fr/estia/mbds/account/CustomerClient.java
@@ -1,15 +1,32 @@
 package fr.estia.mbds.account;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "customer", url = "http://localhost:8080/")
+@FeignClient(name = "CUSTOMER")
 public interface CustomerClient {
     @GetMapping("/customer/{id}")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getDefaultCustomer")
     public Customer getCustomer(@PathVariable Long id);
     @GetMapping("/customers")
+    @CircuitBreaker(name = "customer", fallbackMethod = "getAllCustomers")
     public List<Customer> getCustomers();
+
+    default Customer getDefaultCustomer(Long id, Exception e)
+    {
+        Customer customer = new Customer();
+        customer.setId(id);
+        customer.setFirstName("Not available");
+        customer.setLastName("Not available");
+        customer.setEmail("Not available");
+        return customer;
+    }
+
+    default List<Customer> getAllCustomers(Exception e){
+        return List.of();
+    }
 }
